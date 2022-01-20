d80aec4200f544aecb0f0497334da21b4a34ceb5
Mauricio Gomes <mauricio@edge14.com>
2022-01-19 23:57:29 -0500

    Use larger intervals to avoid noise

--- spec/exec_spec.cr
+++ spec/exec_spec.cr
@@ -3,23 +3,23 @@ require "./spec_helper"
 describe Exec do
   it "executes the block for a given interval" do
     count = Atomic(Int32).new(1)
-    Exec.every(10.milliseconds) do
+    Exec.every(50.milliseconds) do
       count.add(1)
     end
 
-    sleep 50.milliseconds
+    sleep 250.milliseconds
 
     count.get.should be_close(5, 1)
   end
 
   it "handles blocks that run past the interval" do
     count = Atomic(Int32).new(1)
-    Exec.every(10.milliseconds) do
+    Exec.every(50.milliseconds) do
       count.add(1)
-      sleep 50.milliseconds
+      sleep 250.milliseconds
     end
 
-    sleep 50.milliseconds
+    sleep 250.milliseconds
 
     count.get.should be_close(5, 1)
   end
@@ -27,14 +27,14 @@ describe Exec do
   it "handles nesting of exec blocks" do
     count1 = Atomic(Int32).new(1)
     count2 = Atomic(Int32).new(6)
-    Exec.every(10.milliseconds) do
+    Exec.every(50.milliseconds) do
       count1.add(1)
-      Exec.every(10.milliseconds) do
+      Exec.every(50.milliseconds) do
         count2.add(-1)
       end
     end
 
-    sleep 50.milliseconds
+    sleep 250.milliseconds
 
     count1.get.should be_close(5, 1)
     count2.get.should be_close(0, 1)
@@ -42,16 +42,16 @@ describe Exec do
 
   it "handles multiple exec blocks simultaneously" do
     count1 = Atomic(Int32).new(1)
-    Exec.every(10.milliseconds) do
+    Exec.every(50.milliseconds) do
       count1.add(1)
     end
 
     count2 = Atomic(Int32).new(1)
-    Exec.every(5.milliseconds) do
+    Exec.every(25.milliseconds) do
       count2.add(1)
     end
 
-    sleep 50.milliseconds
+    sleep 250.milliseconds
 
     count1.get.should be_close(5, 1)
     count2.get.should be_close(10, 1)
@@ -59,17 +59,17 @@ describe Exec do
 
   it "handles long running exec blocks" do
     count = Atomic(Int32).new(1)
-    Exec.every(10.milliseconds) do
+    Exec.every(50.milliseconds) do
       count.add(1)
     end
 
-    sleep 50.milliseconds
+    sleep 250.milliseconds
     count.get.should eq 5
-    sleep 50.milliseconds
+    sleep 250.milliseconds
     count.get.should be_close(10, 1)
-    sleep 50.milliseconds
+    sleep 250.milliseconds
     count.get.should be_close(15, 1)
-    sleep 500.milliseconds
+    sleep 2500.milliseconds
     count.get.should be_close(65, 5)
   end
 
